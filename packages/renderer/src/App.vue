e19b0dcf444e875b7ca0729f0a89ad6f7c136765
Art Dahm <art@dahm.com>
2025-02-23 20:33:17 -0500

    Converted vue components to tailwind3

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -46,38 +46,3 @@ function showUpdateDateTime() {
   lastUpdated.value = new Date();
 }
 </script>
-
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
-
-main {
-  display: flex;
-  font-family: 'Source Sans Pro', sans-serif;
-  gap: 16px;
-  height: 100vh;
-  justify-content: space-between;
-  margin: auto;
-  max-height: 480px;
-  max-width: 800px;
-  padding: 16px;
-  width: 100vw;
-}
-
-.last-updated {
-  display: flex;
-  flex-direction: column-reverse;
-  flex-grow: 1;
-  font-size: 0.75em;
-}
-
-.left-side {
-  display: flex;
-  flex-direction: column;
-  min-width: 220px;
-  width: 100%;
-}
-
-.right-side {
-    width: 1129px;
-}
-</style>
